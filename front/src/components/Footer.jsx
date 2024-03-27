304e6532210f7a202bae314552e58549a1d7bd7d
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-03-27 14:17:25 +0100

    update front

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -14,7 +14,9 @@ const Footer = () => {
     <Box component='footer' width='100%' mt='auto' padding='20px' display='flex' justifyContent='space-between' position='s' bottom={0}>
       <span>Desarrollado por <Link style={linkStyle} href='https://javihidalgodev.github.io/portfolio/' target='_blank' rel='noopener' underline='none' sx={{'&:hover': {transform: 'translateY(-2px)', textShadow: '0 3px rgba(255, 255, 255, .3)'}}}>javierhidalgo</Link></span>
       <Tooltip title='Repositorio' placement='left' arrow>
-        <GitHubIcon sx={{'&:hover': {fill: '#dbff00'}, transition: '100ms'}} />
+        <Link href='https://github.com/javihidalgodev/shurltener' target='_blank' rel='noopener' color={'#dbff00'}>
+          <GitHubIcon sx={{'&:hover': {fill: '#bcd60f'}, transition: '100ms'}} />
+        </Link>
       </Tooltip>
     </Box>
   )
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import { AppBar, Box, Typography } from "@mui/material"
+import LinkIcon from '@mui/icons-material/Link';
 
 const Navbar = () => {
   return (
-    <AppBar sx={{padding: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'center', boxShadow: 'none'}} position='static' color="transparent">
-      <Box maxWidth='md' width='100%' display='flex' justifyContent='center'>
-        <Typography variant='h6' width='fit-content' textAlign='center' >
-          sh<span>url</span>tener
+    <AppBar sx={{padding: '20px', display: 'flex', justifyContent: 'center', flexDirection: 'row', boxShadow: 'none'}} position='static' color="transparent">
+      <Box maxWidth='md' width='100%' display='flex' justifyContent='start'>
+        <Typography variant='h6' width='fit-content' textAlign='center' display='flex' alignItems='center' gap={1}>
+          <LinkIcon sx={{
+            fill: '#dbff00',
+            '&:hover': {transform: 'rotate(30deg)'},
+            transition: 'transform 150ms'
+          }} />
+          <div>
+            sh<Typography variant='h6' color='#dbff00' display='inline' fontWeight={600}>url</Typography>tener
+          </div>
         </Typography>
       </Box>
     </AppBar>
