6599dac47cbbada83284028ebc13aa3947980b4d
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-03-27 13:45:01 +0100

    api url fix

--- front/src/services/urlServices.js
+++ front/src/services/urlServices.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const getURLs = async () => {
-  const res = await axios.get(`/api/urls`)
+  const res = await axios.get(`https://shurltener-api.vercel.app/api/urls`)
   console.log(res);
 
   return res
 }
 
 const shortURL = async (url) => {
-  const res = await axios.post(`/api/urls`, url)
+  const res = await axios.post(`https://shurltener-api.vercel.app/api/urls`, url)
   return res
 }
 
