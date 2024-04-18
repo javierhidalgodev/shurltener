53b7dbf6bd036f1c353bbc7b2522d2a322ea7509
javierhidalgodev <javierhidalgodev@gmail.com>
2024-04-18 11:26:38 +0200

    Fix

--- front/src/services/loginServices.js
+++ front/src/services/loginServices.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
+const API = 'https://shurltener-api.vercel.app/api/login'
+
 const login = async credentials => {
     const data  = await axios
-    .post('/api/login', credentials, {headers: {'Content-Type': 'application/json'}})
+    .post(API, credentials, {headers: {'Content-Type': 'application/json'}})
     
     return data
 }
