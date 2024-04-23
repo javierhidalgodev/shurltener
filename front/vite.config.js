0ab42eda69633cb9d2e29680de4df5ce288d74b2
javierhidalgodev <javierhidalgodev@gmail.com>
2024-04-23 12:47:35 +0200

    Fix env front

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
+  }
 })
